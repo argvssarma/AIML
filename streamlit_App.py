4654adf4d99fab61ea1cc7465d21020ea6e8f825
argvssarma <33724099+argvssarma@users.noreply.github.com>
2024-12-03 08:40:47 +0530

    Update streamlit_App.py

--- streamlit_App.py
+++ streamlit_App.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -7,7 +8,7 @@ from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-import os
+
 
 st.set_page_config(page_title="Document Genie", layout="wide")
 
